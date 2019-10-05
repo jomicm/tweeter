a3d808e23f29c70807ceb4aadc2bf6ef3f376f93
jomicm <singularity.jmcm@gmail.com>
2019-10-05 16:39:06 +0000

    Tweeter app with toggle buttons for navigation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,98 +1,158 @@
 /*eslint-env jquery*/
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 // eslint-disable-next-line no-undef
 $(document).ready(function() {
+  // Define jQuery elements once so it's more efficient
+  const tweetContainer = $('#tweets-container');
+  const tweetInput = $('#txtIn');
+  const letterCounter = $('.counter');
+  const toggleMenuButton = $('.no-link');
+  const goToSkyButton = $('#go-to-sky');
+  const newTweetForm = $('#post-new-tweet');
 
-  const getElapsedTime = date => {
-    const now = new Date();
-    const diff = now.getTime() - (new Date(date)).getTime();
-    let seconds = diff / 1000;
-    let timeMeasure = 'seconds';
-    let time = seconds;
-    if (seconds > 60 * 60 * 24 * 365) {
-      timeMeasure = 'year';
-      time = Math.floor(seconds / (60 * 60 * 24 * 365));
-    } else if (seconds > 60 * 60 * 24 * 7) {
-      timeMeasure = 'week';
-      time = Math.floor(seconds / (60 * 60 * 24 * 7));
-    } else if (seconds > 60 * 60 * 24) {
-      timeMeasure = 'day';
-      time = Math.floor(seconds / (60 * 60 * 24));
-    } else if (seconds > 60 * 60) {
-      timeMeasure = 'hour';
-      time = Math.floor(seconds / (60 * 60));
-    } else if (seconds > 60) {
-      timeMeasure = 'minute';
-      time = Math.floor(seconds / 60);
-    }
-    return `${time} ${timeMeasure}${time > 1 ? 's' : ''} ago`;
-  };
-
-  const createTweetElement = tweet => {
-    const elapsed = getElapsedTime(tweet.created_at);
-    const resTweet = `<article class="tweet">
-                        <header>
-                          <div class="tweet-identity">
-                            <img class="avatar" src="${tweet.user.avatars}"/>
-                            <span>${tweet.user.name}</span>
-                          </div>
-                          <span class="right-align handle">${tweet.user.handle}</span>
-                        </header>
-                        <div class="tweet-content">
-                          <span>${escape(tweet.content.text)}</span>
-                        </div>
-                        <div class="line"></div>
-                        <footer>
-                          <small>${elapsed}</small>
-                          <div class="right-align">
-                            🇲🇽♼❤️
-                          </div>
-                        </footer>
-                      </article>`;
-    return resTweet;
-  };
-
+  // Appends all tweets to parten element
   const renderTweets = tweets => {
-    $('#tweets-container').html(null);
-    $('#txtIn').val(null);
-    $('.counter').html('140');
+    // Reset all input elements so it is ready for a new tweet
+    tweetContainer.html(null);
+    tweetInput.val(null);
+    letterCounter.html('140');
+    // Appends all elements at once so it's more efficient
     const tweetElements = tweets.map(tweet => createTweetElement(tweet));
-    tweetElements.map(tweetElement => $('#tweets-container').append(tweetElement));
+    tweetContainer.append(tweetElements.join(''));
   };
 
-  const escape =  function(str) {
-    let div = document.createElement('div');
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  };
+  // Overrides form submit method to prevent reloading the page
+  newTweetForm.submit(e => {
+    e.preventDefault();
+    // Validates a correct tweet
+    if (!validateNewTweet()) return;
+    const data  = $(e.currentTarget).serialize();
+    const actionURL = e.currentTarget.action;
+    pushTweets(actionURL, data);
+  });
 
-  const loadTweets = () => {
-    $.ajax('/tweets', { method: 'GET' })
-      .then(tweets => renderTweets(tweets.sort((a, b) => b.created_at - a.created_at)));
+  // AJAX GET - Loads all existing tweets
+  const loadTweets = async() => {
+    try {
+      const tweets = await $.ajax('/tweets', { method: 'GET' });
+      renderTweets(tweets.sort((a, b) => b.created_at - a.created_at));
+    } catch (err) {
+      console.error(err);
+    }
   };
   loadTweets();
 
-  $("#post-new-tweet").submit(function(e) {
+  // AJAX POST - Push new tweets
+  const pushTweets = async(actionURL, data) => {
+    try {
+      await $.ajax(actionURL, { method: 'POST', data });
+      loadTweets();
+    } catch (err) {
+      console.log('Error while Pushig tweets!', err);
+    }
+  };
+
+  // Toggle buttons to navigate to new tweet input
+  toggleMenuButton.on('click', e => {
+    if (!toggleNewTweet) {
+      $('html,body').animate({scrollTop: $('#compose-tweet').offset().top},'slow');
+      $('#arrow-toggle').addClass('rotated');
+    } else {
+      $('html,body').animate({scrollTop: $('html').offset().top},'slow');
+      $('#arrow-toggle').removeClass('rotated');
+    }
+    toggleNewTweet = !toggleNewTweet;
     e.preventDefault();
-    const data = $("#post-new-tweet").serialize();
+    $("#txtIn").focus();
+  });
+  goToSkyButton.on('click', e => {
+    //$('html,body').animate({scrollTop: $('html').offset().top},'slow');
+    $('html,body').animate({scrollTop: $('#compose-tweet').offset().top}, 'slow');
+    e.preventDefault();
+    $("#txtIn").focus();
+  });
+
+  // Display Go To Sky button
+  $(window).on('scroll', () => {
+    if ($(window).scrollTop() > 450) {
+      //goToSkyButton.css('display', 'block');
+      goToSkyButton.fadeIn();
+      toggleMenuButton.fadeOut();
+    } else {
+      goToSkyButton.css('display', 'none');
+      goToSkyButton.fadeOut();
+      toggleMenuButton.fadeIn();
+    }
+  });
+
+  // Validators
+  const validateNewTweet = () => {
     let validationError = '';
-    if (!$('#txtIn').val().length) {
+    if (!tweetInput.val().length) {
       validationError = 'There is no message';
     } else if ($('#txtIn').val().length > 140) {
       validationError = 'Only 140 max length messages allowed';
     }
     if (validationError.length) {
       alert(validationError);
-      return;
+      return false;
     }
-    const actionurl = e.currentTarget.action;
-    $.ajax(actionurl, { method: 'POST', data })
-      .then(() => loadTweets())
-      .fail((req, status, err) => console.log('Error while Pushig tweets!', err, 'st:',status));
-  });
-  
+    return true;
+  };
 });
+
+// Global Variables
+let toggleNewTweet = false;
+
+// Helper infile functions
+const getElapsedTime = date => {
+  const now = new Date();
+  const diff = now.getTime() - (new Date(date)).getTime();
+  let seconds = diff / 1000;
+  let timeMeasure = 'seconds';
+  let time = seconds;
+  if (seconds > 60 * 60 * 24 * 365) {
+    timeMeasure = 'year';
+    time = Math.floor(seconds / (60 * 60 * 24 * 365));
+  } else if (seconds > 60 * 60 * 24 * 7) {
+    timeMeasure = 'week';
+    time = Math.floor(seconds / (60 * 60 * 24 * 7));
+  } else if (seconds > 60 * 60 * 24) {
+    timeMeasure = 'day';
+    time = Math.floor(seconds / (60 * 60 * 24));
+  } else if (seconds > 60 * 60) {
+    timeMeasure = 'hour';
+    time = Math.floor(seconds / (60 * 60));
+  } else if (seconds > 60) {
+    timeMeasure = 'minute';
+    time = Math.floor(seconds / 60);
+  }
+  return `${time} ${timeMeasure}${time > 1 ? 's' : ''} ago`;
+};
+const createTweetElement = tweet => {
+  const elapsed = getElapsedTime(tweet.created_at);
+  const resTweet = `<article class="tweet">
+                      <header>
+                        <div class="tweet-identity">
+                          <img class="avatar" src="${tweet.user.avatars}"/>
+                          <span>${tweet.user.name}</span>
+                        </div>
+                        <span class="right-align handle">${tweet.user.handle}</span>
+                      </header>
+                      <div class="tweet-content">
+                        <span>${escape(tweet.content.text)}</span>
+                      </div>
+                      <div class="line"></div>
+                      <footer>
+                        <small>${elapsed}</small>
+                        <div class="right-align">
+                          🇲🇽♼❤️
+                        </div>
+                      </footer>
+                    </article>`;
+  return resTweet;
+};
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -10,8 +10,9 @@ $(document).ready(function() {
     counter.html(newCount);
     if (newCount < 0) {
       counter.css('color', 'red');
-      // const noSound = new Audio("http://soundbible.com/mp3/Computer%20Error%20Alert-SoundBible.com-783113881.mp3");
-      const noSound = new Audio("http://soundbible.com/mp3/Pew_Pew-DKnight556-1379997159.mp3");
+      const noSound = new Audio("http://soundbible.com/mp3/Computer%20Error%20Alert-SoundBible.com-783113881.mp3");
+      //const noSound = new Audio("http://soundbible.com/mp3/Pew_Pew-DKnight556-1379997159.mp3");
+      // const noSound = new Audio("http://soundbible.com/mp3/PINGAS-Richard-89282878.mp3");
       noSound.play();
     } else counter.css({color:'black'});
   });
